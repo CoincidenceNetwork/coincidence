7f53e6c90bd76f1c59af630397d7bc606c930471
aeither <36173828+aeither@users.noreply.github.com>
2023-11-18 16:24:07 +0300

    use client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,19 +4,22 @@ import { Toaster } from "@/components/ui/toaster";
 
 import "@/styles/globals.css";
 import BottomNavigation from "@/components/bottom-navigation";
+import ClientOnly from "@/components/client-only";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="light"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <Component {...pageProps} />
-      <Toaster />
-      <BottomNavigation></BottomNavigation>
-    </ThemeProvider>
+    <ClientOnly>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Component {...pageProps} />
+        <Toaster />
+        <BottomNavigation></BottomNavigation>
+      </ThemeProvider>
+    </ClientOnly>
   );
 };
 
