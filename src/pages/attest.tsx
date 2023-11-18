d23e6adf4aa9cf0f520afb2be309331478b421c2
aeither <36173828+aeither@users.noreply.github.com>
2023-11-18 19:41:52 +0300

    prep attest

---
+++ src/pages/attest.tsx
@@ -0,0 +1,44 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+import Header from "@/components/header";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import { Textarea } from "@/components/ui/textarea";
+import { PlusCircle } from "lucide-react";
+
+const ProfilePage = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <>
+      <Header></Header>
+      <main className="flex min-h-[calc(100vh-64px)] w-full flex-col px-8 py-20">
+        
+      </main>
+    </>
+  );
+};
+
+export default ProfilePage;
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,5 +1,4 @@
 import Header from "@/components/header";
-import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { Textarea } from "@/components/ui/textarea";
@@ -34,9 +33,9 @@ const ProfilePage = () => {
         <div className="mt-6">
           <div className="flex items-center justify-between">
             <h2 className="text-xl font-bold">Contexts</h2>
-            <Button className="p-2">
+            <button className="p-2">
               <PlusCircle />
-            </Button>
+            </button>
           </div>
           <div className="mt-4 flex items-center space-x-2">
             <Switch id="airplane-mode" />
@@ -48,9 +47,9 @@ const ProfilePage = () => {
         <div className="mt-6">
           <div className="flex items-center justify-between">
             <h2 className="text-xl font-bold">Interests</h2>
-            <Button className="p-2">
+            <button className="p-2">
               <PlusCircle />
-            </Button>
+            </button>
           </div>
           <div className="mt-4 flex items-center space-x-2">
             <Switch />
