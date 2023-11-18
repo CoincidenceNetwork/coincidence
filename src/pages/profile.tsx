aa1f9f7cfb033417b3d191a6ca98082d052ee71e
aeither <36173828+aeither@users.noreply.github.com>
2023-11-18 23:55:38 +0300

    cp

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,9 +1,5 @@
 import Header from "@/components/header";
 import { Label } from "@/components/ui/label";
-import { Switch } from "@/components/ui/switch";
-import { Textarea } from "@/components/ui/textarea";
-import useStore from "@/lib/store";
-import { PlusCircle } from "lucide-react";
 import {
   Sheet,
   SheetContent,
@@ -12,53 +8,74 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { LightNode, createLightNode } from "@waku/sdk";
-import React, { useEffect, useState } from "react";
-
-import { createNode, postUserData } from "../lib/wakunet/waku";
-import { UserDataMessage } from "@/types/alltypes";
-
-export interface ProfileData {
-  name: string;
-  bio: string;
-  img: string;
-  context: string;
-  interests: string[];
-}
 
 const ProfilePage = (wakuNode: LightNode) => {
   const { context, setContext } = useStore();
-
-  // TBD for the form inputs?
-  const [name, setName] = useState("");
-  const [bio, setBio] = useState("");
-  const [img, setImg] = useState("");
-  const [interests, setInterests] = useState([]);
-
-  const handleSubmit = (node: LightNode, event: any) => {
-    event.preventDefault();
-    const profileData = { name, bio, img, context, interests };
-    handleProfileSend(node, profileData);
-  };
-
-  // form function, also saves context to localstorage
-  async function handleProfileSend(node: LightNode, profileData: ProfileData) {
-    const context = profileData.context;
-    // localStorage.setItem("context", context); TBD
-    const userData: UserDataMessage = {
-      name: profileData.name,
-      bio: profileData.bio,
-      img: profileData.img,
-      interests: profileData.interests,
-    };
-    postUserData(node, userData);
-  }
 
   return (
     // TODO make this a form
     <>
       <Header></Header>
       <main className="flex min-h-[calc(100vh-64px)] w-full flex-col px-8 py-20">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="vitalik" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="vitalik" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="airpline"
+              render={({ field }) => (
+                <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
+                  <div className="space-y-0.5">
+                    <FormLabel className="text-base">
+                      Marketing emails
+                    </FormLabel>
+                    <FormDescription>
+                      Receive emails about new products, features, and more.
+                    </FormDescription>
+                  </div>
+                  <FormControl>
+                    <Switch
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
         {/* Section 1 */}
         <div className="flex ">
           <div className="w-2/3">
