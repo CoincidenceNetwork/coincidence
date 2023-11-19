f0949dfc9e9a1cb90b926278c2214b0c41abf679
Orpheus Lummis <o@orpheuslummis.info>
2023-11-19 06:30:00 +0300

    style tweaks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,12 @@ import router from "next/router";
 export default function Home() {
   return (
     <>
-      <main className="text-color flex min-h-[calc(100vh-64px)] w-full flex-col bg-[url('/background.svg')] px-8 py-20 text-slate-800">
+      <main className="text-color flex min-h-[calc(100vh-64px)] w-full flex-col bg-[url('/background.svg')] px-8 py-12 text-slate-800">
         <div className="min-h-screen bg-cover bg-center">
           <div className="container flex w-full flex-col gap-4 py-4">
             <div className="flex w-full flex-col items-center justify-center">
               <img src="/logo.svg" alt="" className="w-32" />
-              <h1 className="scroll-m-20 text-center text-7xl font-extrabold tracking-tight lg:text-5xl">
+              <h1 className="scroll-m-20 rounded-full bg-white bg-opacity-50 p-4 text-center text-7xl font-extrabold tracking-tight lg:text-5xl">
                 Coincidence
               </h1>
               <h2 className="mt-8 scroll-m-20 text-3xl font-semibold tracking-tight text-slate-700 underline">
