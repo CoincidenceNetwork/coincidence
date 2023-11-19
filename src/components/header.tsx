622c338fcb12fea9d755b8c1deb6dfccbadd0cab
Orpheus Lummis <o@orpheuslummis.info>
2023-11-19 06:17:14 +0300

    tweaks

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -27,7 +27,7 @@ const Header = () => {
     <header
       className={`fixed left-0 top-0 z-10 w-full bg-background shadow-md`}
     >
-      <nav className="flex items-center justify-between px-4 py-4">
+      <nav className="flex items-center justify-between px-4 py-4 text-slate-800">
         <Link href="/">
           <div className="text-xl font-bold">Coincidence</div>
         </Link>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,16 +6,15 @@ import router from "next/router";
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-[calc(100vh-64px)] w-full flex-col bg-[url('/background.svg')] px-8 py-20">
+      <main className="text-color flex min-h-[calc(100vh-64px)] w-full flex-col bg-[url('/background.svg')] px-8 py-20 text-slate-800">
         <div className="min-h-screen bg-cover bg-center">
-          {/* The content of your app */}
           <div className="container flex w-full flex-col gap-4 py-4">
             <div className="flex w-full flex-col items-center justify-center">
               <img src="/logo.svg" alt="" className="w-32" />
-              <h1 className="scroll-m-20 text-center text-5xl font-extrabold tracking-tight lg:text-5xl">
-                WELCOME TO COINCIDENCE!
+              <h1 className="scroll-m-20 text-center text-7xl font-extrabold tracking-tight lg:text-5xl">
+                Coincidence
               </h1>
-              <h2 className="mt-8 scroll-m-20 text-3xl font-semibold tracking-tight">
+              <h2 className="mt-8 scroll-m-20 text-3xl font-semibold tracking-tight text-slate-700 underline">
                 How it works
               </h2>
             </div>
@@ -24,12 +23,12 @@ export default function Home() {
               <img src="/icon_addinterest.svg" alt="" className="w-52" />
               <img src="/icon_coincidencepeople.svg" alt="" className="w-52" />
               <img src="/icon_chatfriends.svg" alt="" className="w-52" />
-              {/* <p>its p2p. its private</p> */}
             </div>
             <Button
               onClick={() => {
                 router.push("/profile");
               }}
+              className="mx-auto max-w-xl bg-slate-700 text-lg"
             >
               Get Started
             </Button>
